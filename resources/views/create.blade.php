baf17972acdd606ee3bb70139a5fa94d8bfecb73
hokton-bd <skmhkt.p@gmail.com>
2023-05-04 11:34:05 +0900

    make diary model and migration

---
+++ resources/views/create.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@grammarly/editor-sdk?clientId=client_AzYKa9hJo7wmF86v7C7rEM"></script>
+</head>
+<body>
+    <grammarly-editor-plugin>
+        <textarea></textarea>
+      </grammarly-editor-plugin>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/create', function() {
+    return view('create');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
